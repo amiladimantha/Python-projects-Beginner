e5d6f055fb422043fcb402eae7777743b667a40d
Amila Dimantha <amiladimantha123@gmail.com>
2023-10-03 15:00:07 +0530

    string concat

---
+++ String Concatanation/string_concat.py
@@ -0,0 +1,7 @@
+string1  = "Adding Strings" #any string you want
+
+#there are many ways to do this..
+print("The name of the projects is " + string1)
+print("The name of the projects is",string1)#when we use (,) a space will be automatically added to the end of the previous string before the next string is added
+print("The name of the projects is {}".format(string1))
+print(f"The name of the projects is {string1}")
